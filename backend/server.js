c2388eeb6383d066c293926cc5ab1522fe80851e
AbhinavRajeshKalvi <abhinav.rajesh@kalvium.community>
2024-02-09 12:08:42 +0530

    DBConnection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
-const express = require("express")
+const express = require("express");
+const DBconnection = require("./DBconnection/DBconnection");
 const app = express()
-
+const { connection, default: mongoose } = require("mongoose")
 const port = process.env.PORT || 3000;
+const dotenv = require('dotenv').config()
+
+DBconnection()
+
+app.get("/",(req,res)=>{
+    res.send(mongoose.connection.readyState === 1 ? "Database Connnected " : "Database Disonnected ")
+})
 
 app.get('/ping', (req,res)=>{
     res.send("pong")
