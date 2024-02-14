b92700d45a583aa51fe4d45faac06667ae8f0432
AbhinavRajeshKalvi <abhinav.rajesh@kalvium.community>
2024-02-14 15:44:44 +0530

    made changes to joi

--- backend/joi.js
+++ backend/joi.js
@@ -1,9 +1,9 @@
-const joi=require("joi");
+const joi = require("joi");
 
 const validation=joi.object({
-    title:joi.string().min(8).max(30).required(),
+    title:joi.string().min(5).max(30).required(),
     image:joi.string().required(),
-    failRating:joi.number().integer().min(0).max(10).required(),
+    rating:joi.number().integer().min(0).max(10).required(),
 })
 
 module.exports=validation
