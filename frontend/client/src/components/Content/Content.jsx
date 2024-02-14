9c1d396eb963f78fb938beb30d0fe9b3b917d051
AbhinavRajeshKalvi <abhinav.rajesh@kalvium.community>
2024-02-14 14:44:11 +0530

    added api link

--- frontend/client/src/components/Content/Content.jsx
+++ frontend/client/src/components/Content/Content.jsx
@@ -16,7 +16,7 @@ const Content = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(''); 
+        const response = await axios.get('https://unusualscoops.onrender.com/api'); 
         console.log(response.data)
       } catch (error) {
         console.error(error)
